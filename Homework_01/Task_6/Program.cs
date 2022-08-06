a04bace1150f191404216697af08543bd4dfd165
10vital08 <vital.lushnikov@mail.ru>
2022-08-06 11:34:00 +0300

    Add task 6

---
+++ Homework_01/Task_6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0){
+    Console.WriteLine("Число чётное");
+}
+if (number % 2 != 0){
+     Console.WriteLine("Число нечётное");
+}
