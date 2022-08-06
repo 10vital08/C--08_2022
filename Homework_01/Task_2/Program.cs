c60791b9624dda76d1c3bf47ee3e9e0a0c2e8e6f
10vital08 <vital.lushnikov@mail.ru>
2022-08-06 10:11:55 +0300

    Add Task 2

---
+++ Homework_01/Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2){
+    Console.WriteLine("Максимальное число: " + number_1);
+}
+if (number_2 > number_1){
+    Console.WriteLine("Максимальное число: " + number_2);
+}
+else if (number_1 == number_2){
+    Console.WriteLine("Введите разные числа");
+}
