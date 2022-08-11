d2f9ec96e26885c5c419f43e659ac85aeb20e78d
10vital08 <vital.lushnikov@mail.ru>
2022-08-11 18:26:49 +0300

    add Task 10

---
+++ Homework_02/Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿void PrintFirstNumber(int num)
+{
+    int result = num / 10;//отсечение третьей цифры
+    result = result % 10;//найти остаток от деления на 10
+    Console.WriteLine(result);
+}
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintFirstNumber(number);
