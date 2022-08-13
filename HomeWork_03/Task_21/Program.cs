4314dd35220832f0fcfe728ee79119e3d5e59437
10vital08 <vital.lushnikov@mail.ru>
2022-08-13 12:50:04 +0300

    Add Task 21

---
+++ HomeWork_03/Task_21/Program.cs
@@ -0,0 +1,22 @@
+﻿double SearchDistance(int Ax, int Ay, int Az, int Bx, int By, int Bz)
+{
+    double sqrtCoord = Math.Sqrt(Math.Pow((Bx - Ax), 2) + Math.Pow((By - Ay), 2) + Math.Pow((Bz -Az), 2));
+    return sqrtCoord;
+}
+
+Console.Write("Задайте x первой координаты: ");
+int coordAx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте у первой координаты: ");
+int coordAy = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте z первой координаты: ");
+int coordAz = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте x второй координаты: ");
+int coordBx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте у второй координаты: ");
+int coordBy = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте z второй координаты: ");
+int coordBz = Convert.ToInt32(Console.ReadLine());
+
+double result = SearchDistance(coordAx, coordAy, coordAz, coordBx, coordBy, coordBz);
+Console.WriteLine(result);
