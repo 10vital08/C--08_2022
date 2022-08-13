a3c054b94b9c04d2f0d23dbdb5b38bf9ebf73927
10vital08 <vital.lushnikov@mail.ru>
2022-08-13 13:00:21 +0300

    Add Task 23

---
+++ HomeWork_03/Task_23/Program.cs
@@ -0,0 +1,14 @@
+﻿int ShowCubes(int n)
+{
+    int cube = Convert.ToInt32(Math.Pow(n, 3));
+    return cube;
+}
+
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.Write($"{ShowCubes(i)} ");
+}
