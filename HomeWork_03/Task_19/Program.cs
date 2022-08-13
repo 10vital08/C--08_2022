812bd8882f64bf5ad3c4d6ae6e9f1405c17a7bcc
10vital08 <vital.lushnikov@mail.ru>
2022-08-13 12:38:28 +0300

    Add Task 19

---
+++ HomeWork_03/Task_19/Program.cs
@@ -0,0 +1,21 @@
+﻿bool CheckPolyndrom(string num)
+{
+    char[] reverseNum = num.ToCharArray();//перевести строку в массив символов
+    Array.Reverse(reverseNum);//развернуть массив
+    string polyndromNum = new string(reverseNum);//заполнить строку массивом символов
+    if (num == polyndromNum)
+    {
+        return true;
+    }
+    else return false;
+}
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+if (number.Length == 5)
+{
+    Console.WriteLine(CheckPolyndrom(number));
+}
+else Console.WriteLine("Вы ввели не пятизначное число");
+
+
