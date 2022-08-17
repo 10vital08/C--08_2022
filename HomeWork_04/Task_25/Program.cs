8d8881f2deb36cad526f09f4d2a57cc07e1e7a8c
10vital08 <vital.lushnikov@mail.ru>
2022-08-17 09:16:39 +0300

    Add Task 25

---
+++ HomeWork_04/Task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿int RaisePower(int A, int B)
+{
+    int result = Convert.ToInt32(Math.Pow(A, B));
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int resultNumber = -1;
+
+if (numberB > 0)
+{
+    resultNumber = RaisePower(numberA, numberB);
+    Console.WriteLine($"A в степени B = {resultNumber}");
+}
+else Console.WriteLine("Введите натуральные числа");
