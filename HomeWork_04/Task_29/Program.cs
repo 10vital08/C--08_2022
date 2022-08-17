c9c12feaeabc1ef0d4d3d81e9139c6736614a5af
10vital08 <vital.lushnikov@mail.ru>
2022-08-17 10:44:01 +0300

    Add Task 29

---
+++ HomeWork_04/Task_29/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] GenerateNumber(int N)
+{
+    int[] array = new int[N];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 101);
+    }
+    return array;
+}
+
+void PrintArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array1[i]} ");
+    }
+}
+
+Console.Write("Введите число элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = GenerateNumber(n);
+PrintArray(arr);
