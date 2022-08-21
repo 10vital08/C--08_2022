ede1cfb90c9bd4bca8b1c5fbba4027701ae68c73
10vital08 <vital.lushnikov@mail.ru>
2022-08-21 12:17:27 +0300

    Task 36 completed

--- HomeWork_05/Task_36/Program.cs
+++ HomeWork_05/Task_36/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+
+PrintArray(FillArray(array, length));//вывести массив, заполненный случайными числами
+int sumNotEven = CountEven(array);//посчитать количество элементов с нечётными индексами
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов массива с нечётными индексами: {sumNotEven}");
+
+int[] FillArray(int[] arr, int n)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(-100, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int CountEven(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i = i + 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
