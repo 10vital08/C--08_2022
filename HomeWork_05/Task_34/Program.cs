88cd62f1b07736d61cc1332ff0aa6fbdff44d041
10vital08 <vital.lushnikov@mail.ru>
2022-08-21 12:01:57 +0300

    Task 34 completed

---
+++ HomeWork_05/Task_34/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+
+PrintArray(FillArray(array, length));//вывести массив, заполненный случайными трёхзначными числами
+int countEv = CountEven(array);//посчитать количество чётных чисел
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел: {countEv}");
+
+int[] FillArray(int[] arr, int n)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int CountEven(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
