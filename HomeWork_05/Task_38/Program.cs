963a8cada42b4774070f1235e0ea71c7b382a71b
10vital08 <vital.lushnikov@mail.ru>
2022-08-21 12:30:47 +0300

    Task 38 completed

--- HomeWork_05/Task_38/Program.cs
+++ HomeWork_05/Task_38/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[] array = { 1, 3, 4.1, -5.3, 6.8 };
+
+PrintArray(array);//вывести массив
+double difference = FindDifference(array);//посчитать разницу между минимальным и максимальным элементом массива
+Console.WriteLine();
+Console.WriteLine($"Разница между минимальным и максимальным элементом равна: {difference}");
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} | ");
+    }
+}
+
+double FindDifference(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+    
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max - min;
+}
