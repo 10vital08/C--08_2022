7a730db96bc5abf2dccdf62e0ecc8ef040b5620d
10vital08 <vital.lushnikov@mail.ru>
2022-09-02 22:54:50 +0300

    Task 64 and 66 completed

---
+++ HomeWork_09/Task_64/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void PrintNumbers(int index, int num)
+{
+    Console.Write($"{index} ");
+    if (index < num)
+    {
+        PrintNumbers(index + 1, num);
+    }
+
+}
+
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbers(m, n);
---
+++ HomeWork_09/Task_66/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+int FindNaturalSum(int index, int num, int sum)
+{
+    if (index < 1)
+    {
+        index = 1;
+    }
+    if (index - num != 0)
+    {
+        sum += index;
+        return FindNaturalSum(index + 1, num, sum);
+    }
+    else return sum + index;
+}
+
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < m)
+{
+    Console.Write("Задайте второе число больше первого");
+}
+else
+{
+    int sum = FindNaturalSum(m, n, 0);
+    Console.WriteLine($"Сумма натуральных элементов от {m} до {n} = {sum}");
+}
+
